c84aaa0bd87c3952507fc12b1487fcab194c1e2b
Mahmoud-Git123 <123002184+Mahmoud-Git123@users.noreply.github.com>
2023-05-09 17:55:53 +0100

    Update AirHockey.java

--- AirHockey.java
+++ AirHockey.java
@@ -1,12 +1,15 @@
 public class AirHockey{
   public static void main (String args[]){
-    GameArena gameObj = new GameArena(900, 500, 50, true);
+    
+    //CREATING OBJECTS
+    
+    GameArena gameObj = new GameArena(900, 500, true);
     
     Ball ballObj1 = new Ball (175, 250, 50, "BLUE", 5);
     Ball ballObj2 = new Ball (725, 250, 50, "BLUE", 5);
     
-    Ball centerObj1 = new Ball (450, 250, 70, "WHITE" , 3);
-    Ball centreObj2 = new Ball (450, 250, 72, "BLUE", 2);
+    Ball centerObj1 = new Ball (450, 250, 70, "WHITE" , 3); //center circle
+    Ball centerObj2 = new Ball (450, 250, 72, "BLUE", 2); //center circle (blue stroke)
     
     Ball puckObj = new Ball(450, 250, 15, "BLACK", 6);
     
@@ -19,6 +22,41 @@ public class AirHockey{
     Line lineObj = new Line (450, 87.5, 450, 412.5, 1, "BLUE", 1);
     
     Text textObj = new Text ("Welcome to Air Hockey!", 20, 50, 50, "WHITE", 1);
+    
+    //ADDING
+    
+    gameObj.addRectangle(goalObj1); //adding goal rectangle 1
+    gameObj.addRectangle(goalObj2); //adding goal rectangle 2
+    
+    blueRecObj.setXPosition(gameObj.getWidth() - blueRecObj.getWidth()/2); //centering the blue rectangle
+    blueRecObj.setYPosition(gameObj.getHeight() - blueRecObj.getHeight()/2); //centering the blue rectangle
+    
+    whiteRecObj.setXPosition(gameObj.getWidth() - whiteRecObj.getWidth()/2); //centering the white rectangle
+    whiteRecObj.setYPosition(gameObj.getHeight() - whiteRecObj.getHeight()/2); //centering the white rectangle
+    
+    gameObj.addRectangle(blueRecObj); //adding blue rectangle
+    gameObj.addRectangle(whiteRecObj); //adding white rectangle
+    
+    gameObj.addText(textObj); //adding text
+    
+    gameObj.addBall(ballObj1); //adding ball 1
+    gameObj.addBall(ballObj2); //adding ball 2
+    
+    gameObj.addBall(centerObj1); //adding center circle (white) 1
+    gameObj.addBall(centerObj2); //adding center circle (blue) 2
+    
+    gameObj.addBall(puckObj); //adding puck
+    
+    gameObj.addLine(lineObj); //adding line
+    
+    //MOVEMENT
+    
+      //BALL 1
+
+      //BALL 2
+    
+      
+    
     
   }
 }
