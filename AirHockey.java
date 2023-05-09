87a3df819b63aae831738d5987962bc9ca802006
Mahmoud-Git123 <123002184+Mahmoud-Git123@users.noreply.github.com>
2023-05-09 16:56:37 +0100

    Update AirHockey.java

--- AirHockey.java
+++ AirHockey.java
@@ -1,1 +1,25 @@
-
+public class AirHockey{
+  public static void main (String args[]){
+    GameArena gameObj = new GameArena(900, 500, 50, true);
+    
+    Ball ballObj1 = new Ball (175, 250, 50, "BLUE", 5);
+    Ball ballObj2 = new Ball (725, 250, 50, "BLUE", 5);
+    
+    Ball centerObj1 = new Ball (450, 250, 70, "WHITE" , 3);
+    Ball centreObj2 = new Ball (450, 250, 72, "BLUE", 2);
+    
+    Ball puckObj = new Ball(450, 250, 15, "BLACK", 6);
+    
+    Rectangle blueRecObj = new Rectangle (0, 0, 800, 350, "BLUE");
+    Rectangle whiteRecObj = new Rectangle (0, 0, 775, 325, "WHITE");
+    
+    Rectangle goalObj1 = new Rectangle (62.5, 175, 10,150, "GREY", 4);
+    Rectangle goalObj2 = new Rectangle (827.5, 175, 10, 150, "GREY", 4);
+                                
+    Line lineObj = new Line (450, 87.5, 450, 412.5, 1, "BLUE", 1);
+    
+    Text textObj = new Text ("Welcome to Air Hockey!", 20, 50, 50, "WHITE", 1);
+    
+          
+                                
+      
